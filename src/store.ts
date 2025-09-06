3ba07d4b4b957b1b32a27d062136f6551dd5ed3a
prashantkhedkar <justprashant5@gmail.com>
2025-09-06 13:39:59 +0400

    LL
    LL

---
+++ src/store.ts
@@ -0,0 +1,21 @@
+// Below code is standard definition of Redux Store
+import { configureStore } from "@reduxjs/toolkit";
+import { useDispatch, TypedUseSelectorHook, useSelector } from "react-redux";
+import { globalSlice } from "./app/modules/services/globalSlice";
+import { feedbackSlice } from "./app/modules/services/feedbackSlice";
+import notificationReducer from "./app/modules/services/notificationSlice";
+export const store = configureStore({
+  // create and add new slice actions in object which will be exposed to components via the custom hooks useAppDispatch & useAppSelector
+  reducer: {
+    globalgeneric: globalSlice.reducer,
+    feedback: feedbackSlice.reducer,
+    notifications: notificationReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+//Create custom hooks for Dispatch and Selector
+export const useAppDispatch = () => useDispatch<AppDispatch>(); // to update store
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector; // to read from store
