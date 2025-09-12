ad26a3404994d750bd42dcee25bf091300baeb1b
PrashantKhedkat <justprashant5@gmail.com>
2025-09-12 10:34:41 +0400

    Checkin

--- src/app/observation/pages/ObservationPage.tsx
+++ src/app/observation/pages/ObservationPage.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { ObservationSteppers } from '../components/ObervationSteppers';
 import './ObservationPage.css';
+import ActionsDisplay from '../components/ActionsDisplay';
 
 const ObservationPage: React.FC = () => {
   return (
     <div className="container-fluid observation-page-container">
       <div className="card observation-page-card">
         <div className="card-body observation-page-card-body">
+          {/* <ActionsDisplay 
+                          recommendationId={9}
+                        /> */}
           <ObservationSteppers />
         </div>
       </div>
